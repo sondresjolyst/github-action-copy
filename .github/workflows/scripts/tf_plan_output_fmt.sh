70ed8cb4b062a01cb7f99f35e26fffe17b295d8e
sondresjolyst <sonyslyst@gmail.com>
2023-01-12 13:38:13 +0100

    a

--- .github/workflows/scripts/tf_plan_output_fmt.sh
+++ .github/workflows/scripts/tf_plan_output_fmt.sh
@@ -5,10 +5,10 @@ arg=("${@:2}")
 tfplan=$(terraform -chdir="$path" plan "${arg[@]}")
 # strArr=("$(IFS=$'\n'; echo $tfplan)")
 
-createArr=()
-destroyArr=()
-updateArr=()
-out=()
+# createArr=()
+# destroyArr=()
+# updateArr=()
+# out=()
 
 while read -r line; do
   if [[ -z $line ]]; then 
@@ -17,24 +17,25 @@ while read -r line; do
   fi
   if [[ $line =~ "will be created" ]]; then
     echo "${line}"
-    createArr+=("${line//"# "/"+ "}")
-    echo "--createArr loop--"
-    echo "${createArr[@]}"
-    echo "--"
-  elif [[ $line =~ "will be destroyed" ]]; then
-    destroyArr+=("${line//"# "/"- "}")
-  elif [[ $line =~ "will be updated in-place" ]]; then
-    updateArr+=("${line//"# "/"~ "}")
+    # createArr+=("${line//"# "/"+ "}")
+    # echo "--createArr loop--"
+    # echo "${createArr[@]}"
+    # echo "--"
   fi
+  # elif [[ $line =~ "will be destroyed" ]]; then
+  #   destroyArr+=("${line//"# "/"- "}")
+  # elif [[ $line =~ "will be updated in-place" ]]; then
+  #   updateArr+=("${line//"# "/"~ "}")
+  # fi
 done < <(echo "${tfplan}")
 
-echo "--createArr--"
-echo "${createArr[@]}"
-echo "--"
+# echo "--createArr--"
+# echo "${createArr[@]}"
+# echo "--"
 
-echo "--tfplan--"
-echo "${tfplan}"
-echo "--"
+# echo "--tfplan--"
+# echo "${tfplan}"
+# echo "--"
 
 # numberOfChanges="${#createArr[@]} + ${#destroyArr[@]} + ${#updateArr[@]}"
 
@@ -63,7 +64,7 @@ echo "--"
 # printf ""
 
 
-out+=("${createArr[@]}" "${destroyArr[@]}" "${updateArr[@]}")
+# out+=("${createArr[@]}" "${destroyArr[@]}" "${updateArr[@]}")
 
 # # rm out.txt
 # echo "${test[@]}" | tr -d '\n' > out.txt
@@ -89,4 +90,4 @@ out+=("${createArr[@]}" "${destroyArr[@]}" "${updateArr[@]}")
 # echo "tf-plan-output=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" >> $GITHUB_OUTPUT # works
 # echo "tf-plan-output=$(echo "+ azurerm_private_dns_zone_virtual_network_link.cluster_link[0] will be created \ ")" >> $GITHUB_OUTPUT # works
 # echo "tf-plan-output=$(echo "+ azurerm_private_dns_zone_virtual_network_link.cluster_link[0] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[1] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[2] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[3] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[4] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[5] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[6] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[7] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[8] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[9] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[10] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[11] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[12] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[13] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[14] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[15] will be created + azurerm_private_dns_zone_virtual_network_link.cluster_link[16] will be created + azurerm_redis_cache.redis_cache_web_console[0] will be created + azurerm_redis_cache.redis_cache_web_console[1] will be created + null_resource.add_whitelist_acr will be created + null_resource.delete_whitelist_acr will be created + module.aks.azurerm_kubernetes_cluster.kubernetes_cluster will be created + module.aks.azurerm_kubernetes_cluster_node_pool.nodepools[0] will be created + module.aks.azurerm_kubernetes_cluster_node_pool.nodepools[1] will be created + module.aks.azurerm_kubernetes_cluster_node_pool.nodepools[2] will be created + module.aks.azurerm_kubernetes_cluster_node_pool.nodepools[3] will be created + module.aks.azurerm_network_security_group.nsg_cluster will be created + module.aks.azurerm_public_ip.pip_ingress will be created + module.aks.azurerm_subnet.subnet_cluster will be created + module.aks.azurerm_virtual_network.vnet_cluster will be created + module.aks.azurerm_virtual_network_peering.cluster_to_hub will be created + module.aks.azurerm_virtual_network_peering.hub_to_cluster will be created + module.aks.random_id.four_byte will be created")" >> $GITHUB_OUTPUT
-echo "tf-plan-output=$(echo $foo)" >> $GITHUB_OUTPUT
+# echo "tf-plan-output=$(echo $foo)" >> $GITHUB_OUTPUT
