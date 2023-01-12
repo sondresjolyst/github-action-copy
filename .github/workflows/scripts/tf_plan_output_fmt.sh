734337c563ae78b67182130a889836b91d05446b
sondresjolyst <sonyslyst@gmail.com>
2023-01-12 08:18:17 +0100

    a

--- .github/workflows/scripts/tf_plan_output_fmt.sh
+++ .github/workflows/scripts/tf_plan_output_fmt.sh
@@ -3,26 +3,24 @@
 path=$1
 arg=("${@:2}")
 tfplan=$(terraform -chdir="$path" plan "${arg[@]}")
+# strArr=("$(IFS=$'\n'; echo $tfplan)")
 
 createArr=()
 destroyArr=()
 updateArr=()
-
-# echo "1"
+out=()
 
 while read -r line; do
   if [[ $line =~ "will be created" ]]; then
-    createArr+=("${line//"# "/"+ "}")
+    createArr+=("${line//"# "/"+ "} \\")
   elif [[ $line =~ "will be destroyed" ]]; then
-    destroyArr+=("${line//"# "/"- "}")
+    destroyArr+=("${line//"# "/"- "} \\")
   elif [[ $line =~ "will be updated in-place" ]]; then
-    updateArr+=("${line//"# "/"~ "}")
+    updateArr+=("${line//"# "/"~ "} \\")
   fi
 done <<<"$tfplan"
 
-# echo "2"
-
-numberOfChanges="${#createArr[@]} + ${#destroyArr[@]} + ${#updateArr[@]}"
+# numberOfChanges="${#createArr[@]} + ${#destroyArr[@]} + ${#updateArr[@]}"
 
 # if [ ${#createArr[@]} -gt 0 ]; then
 #   printf ""
@@ -48,67 +46,10 @@ numberOfChanges="${#createArr[@]} + ${#destroyArr[@]} + ${#updateArr[@]}"
 # fi
 # printf ""
 
-out=()
-out+=("${createArr[@]}")
-out+=("${destroyArr[@]}")
-out+=("${updateArr[@]}")
 
-foobar="+ azurerm_private_dns_zone_virtual_network_link.cluster_link[0] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[1] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[2] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[3] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[4] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[5] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[6] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[7] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[8] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[9] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[10] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[11] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[12] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[13] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[14] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[15] will be created
-+ azurerm_private_dns_zone_virtual_network_link.cluster_link[16] will be created
-+ azurerm_redis_cache.redis_cache_web_console[0] will be created
-+ azurerm_redis_cache.redis_cache_web_console[1] will be created
-+ null_resource.add_whitelist_acr will be created
-+ null_resource.delete_whitelist_acr will be created
-+ module.aks.azurerm_kubernetes_cluster.kubernetes_cluster will be created
-+ module.aks.azurerm_kubernetes_cluster_node_pool.nodepools[0] will be created
-+ module.aks.azurerm_kubernetes_cluster_node_pool.nodepools[1] will be created
-+ module.aks.azurerm_kubernetes_cluster_node_pool.nodepools[2] will be created
-+ module.aks.azurerm_kubernetes_cluster_node_pool.nodepools[3] will be created
-+ module.aks.azurerm_network_security_group.nsg_cluster will be created
-+ module.aks.azurerm_public_ip.pip_ingress will be created
-+ module.aks.azurerm_subnet.subnet_cluster will be created
-+ module.aks.azurerm_virtual_network.vnet_cluster will be created
-+ module.aks.azurerm_virtual_network_peering.cluster_to_hub will be created
-+ module.aks.azurerm_virtual_network_peering.hub_to_cluster will be created
-+ module.aks.random_id.four_byte will be created"
+out+=("${createArr[@]}" "${destroyArr[@]}" "${updateArr[@]}")
 
-foo=$(jq --compact-output --null-input '$ARGS.positional' --args -- "${out[@]}")
-bar=$( IFS=$'\n'; echo "${out[*]}" )
-# barbar=$(echo $bar | tr '\n' '\\')
-# barfoo=$(echo ${foobar} | tr '\n' '\\')
-# JSON="[$(echo $out | sed 's/ /, /g')]"
+rm out.txt
+echo "${out[@]}" >> out.txt
 
-# echo "bar"
-# echo "$bar" | tr '\n' '  '
-echo "${bar//$'\n'/$' \\ '}"
-saab=$(echo "${bar//$'\n'/$' \\ '}" | base64)
-echo "foo=${saab}" >> $GITHUB_OUTPUT
-# echo "SELECTED_COLOR=green" >> $GITHUB_OUTPUT
-# echo "tf-planstdout=$(echo "${bar//$'\n'/$' \\ '}")" >> $GITHUB_OUTPUT
-# echo "$barfoo"
-# echo "$barbar"
-# echo "tf-planstdout=$(echo "$barfoo")" >> $GITHUB_OUTPUT
-
-# echo "stdout=$(echo ${foo})" >> $GITHUB_OUTPUT
-# echo "bar=bar" >> $GITHUB_OUTPUT
-# echo "foo=$(echo "${foobar}")" >> $GITHUB_OUTPUT
-# {
-#   echo 'foo<<EOF'
-#   echo "${foobar}"
-#   echo 'EOF'
-# } >> $GITHUB_ENV
+echo "tf-planstdout=$(cat out.txt)" >> $GITHUB_OUTPUT
